1a2170e740f10375ce45437b26b1f6487723fdac
Daksh Jain <dakshbjain@gmail.com>
2022-01-29 18:38:13 -0500

    more updates to the application

--- src/app/pipes/sum.pipe.ts
+++ src/app/pipes/sum.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SumPipe implements PipeTransform {
 
-  transform(items: any[], attr: string): unknown {
+  transform(items: any[], attr: string): any {
     return items.reduce((a, b) => a + b[attr], 0);
   }
 
